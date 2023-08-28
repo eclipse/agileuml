ec8ee2a28effd4eec74fcd29914762a322faa9b8
Kevin Lano <kevin.lano@kcl.ac.uk>
2023-08-28 16:39:22 +0100

    Binomial and LogNorm distributions

--- libraries/OclRandom.java
+++ libraries/OclRandom.java
@@ -18,6 +18,18 @@ class OclRandom { static ArrayList<OclRandom> OclRandom_allInstances = new Array
   int ix = 0;
   int iy = 0;
   int iz = 0;
+  String distribution = "uniform"; 
+
+  double bernoulliP = 0.0; 
+  int binomialN = 1; 
+  double binomialP = 0.0; 
+  double normalMean = 0.0; 
+  double normalVariance = 1.0; 
+  private double uniformLower = 0.0; 
+  private double uniformUpper = 1.0; 
+
+  private static OclRandom _defaultInstanceOclRandom = null; 
+
   String oclrandomId = ""; /* primary */
   static Map<String,OclRandom> OclRandom_index = new HashMap<String,OclRandom>();
 
@@ -27,17 +39,63 @@ class OclRandom { static ArrayList<OclRandom> OclRandom_allInstances = new Array
     result = new OclRandom();
     OclRandom.OclRandom_index.put(oclrandomIdx,result);
     result.oclrandomId = oclrandomIdx;
-    return result; }
+    result.distribution = "uniform";
+    return result; 
+  }
 
   static void killOclRandom(String oclrandomIdx)
   { OclRandom rem = OclRandom_index.get(oclrandomIdx);
-    if (rem == null) { return; }
+    if (rem == null) 
+    { return; }
     ArrayList<OclRandom> remd = new ArrayList<OclRandom>();
     remd.add(rem);
     OclRandom_index.remove(oclrandomIdx);
     OclRandom_allInstances.removeAll(remd);
   }
 
+  public static OclRandom defaultInstanceOclRandom()
+  { if (OclRandom._defaultInstanceOclRandom == null)
+    { 
+      OclRandom._defaultInstanceOclRandom = OclRandom.newOclRandom();
+    }
+    return OclRandom._defaultInstanceOclRandom; 
+  }
+
+  public void setix(int ix_x) { ix = ix_x;  }
+
+  public void setiy(int iy_x) { iy = iy_x;  }
+
+  public void setiz(int iz_x) { iz = iz_x;  }
+
+  public void setdistribution(String distribution_x) { distribution = distribution_x;  }
+
+  public void setbernoulliP(double bernoulliP_x) { bernoulliP = bernoulliP_x;  }
+
+  public void setnormalMean(double normalMean_x) { normalMean = normalMean_x;  }
+
+  public void setnormalVariance(double normalVariance_x) { normalVariance = normalVariance_x;  }
+
+  public void setuniformLower(double uniformLower_x) { uniformLower = uniformLower_x;  }
+
+  public void setuniformUpper(double uniformUpper_x) { uniformUpper = uniformUpper_x;  }
+
+  public int getix() { return ix; }
+
+  public int getiy() { return iy; }
+
+  public int getiz() { return iz; }
+
+  public String getdistribution() { return distribution; }
+
+  public double getbernoulliP() { return bernoulliP; }
+
+  public double getnormalMean() { return normalMean; }
+
+  public double getnormalVariance() { return normalVariance; }
+
+  public double getuniformLower() { return uniformLower; }
+
+  public double getuniformUpper() { return uniformUpper; }
 
   public static OclRandom newOclRandom()
   {
@@ -62,6 +120,77 @@ class OclRandom { static ArrayList<OclRandom> OclRandom_allInstances = new Array
     rd.iz = (int) (n % 30323);
     result = rd;
     return result;
+  }
+
+  public static OclRandom newOclRandom_Seed(long n)
+  {
+    OclRandom result = null;
+    OclRandom rd = null;
+    rd = OclRandom.createOclRandom();
+    rd.ix = (int) (n % 30269);
+    rd.iy = (int) (n % 30307);
+    rd.iz = (int) (n % 30323);
+    result = rd;
+    return result;
+  }
+
+  public static OclRandom newOclRandomBernoulli(double p)
+  {
+    OclRandom rd = OclRandom.createOclRandom();
+    rd.ix = 1001;
+    rd.iy = 781;
+    rd.iz = 913;
+    rd.distribution = "bernoulli";
+    rd.bernoulliP = p;
+    return rd;
+  }
+
+  public static OclRandom newOclRandomBinomial(int n, double p)
+  {
+    OclRandom rd = OclRandom.createOclRandom();
+    rd.ix = 1001;
+    rd.iy = 781;
+    rd.iz = 913;
+    rd.distribution = "binomial";
+    rd.binomialN = n; 
+    rd.binomialP = p;
+    return rd;
+  }
+
+  public static OclRandom newOclRandomNormal(double mu, double vari)
+  {
+    OclRandom rd = OclRandom.createOclRandom();
+    rd.ix = 1001;
+    rd.iy = 781;
+    rd.iz = 913;
+    rd.distribution = "normal";
+    rd.normalMean = mu;
+    rd.normalVariance = vari; 
+    return rd;
+  }
+
+  public static OclRandom newOclRandomLogNormal(double mu, double vari)
+  {
+    OclRandom rd = OclRandom.createOclRandom();
+    rd.ix = 1001;
+    rd.iy = 781;
+    rd.iz = 913;
+    rd.distribution = "lognormal";
+    rd.normalMean = mu;
+    rd.normalVariance = vari; 
+    return rd;
+  }
+
+  public static OclRandom newOclRandomUniform(double lwr,double upr)
+  { 
+    OclRandom rd = OclRandom.createOclRandom();
+    rd.setix(1001);
+    rd.setiy(781);
+    rd.setiz(913);
+    rd.setdistribution("uniform");
+    rd.setuniformLower(lwr);
+    rd.setuniformUpper(upr);
+    return rd;
   }
 
 
@@ -116,7 +245,7 @@ class OclRandom { static ArrayList<OclRandom> OclRandom_allInstances = new Array
     double result = 0.0;
     double d = 0.0;
     d = this.nrandom();
-    result = (d / 3.0 - 0.5);
+    result = d*2.0 - 3.0;
     return result;
   }
 
@@ -130,14 +259,12 @@ class OclRandom { static ArrayList<OclRandom> OclRandom_allInstances = new Array
     return result;
   }
 
-
   public int nextInt()
   {
     int result = 0;
     result = this.nextInt(2147483647);
     return result;
   }
-
 
   public long nextLong()
   {
@@ -147,7 +274,6 @@ class OclRandom { static ArrayList<OclRandom> OclRandom_allInstances = new Array
     result = (long) Math.floor(d * 9223372036854775807L);
     return result;
   }
-
 
   public boolean nextBoolean()
   {
@@ -160,6 +286,95 @@ class OclRandom { static ArrayList<OclRandom> OclRandom_allInstances = new Array
     }
     return result;
   }
+
+  public int nextBernoulli(double p)
+  {
+    int result = 1;
+    double d = 0.0;
+    d = this.nextDouble();
+    if (d > p)
+    {
+      result = 0;
+    }
+    return result;
+  }
+
+  public int nextBinomial(int n, double p) 
+  { int res = 0; 
+    for (int ind = 0; ind < n; ind++)  
+    { double d = this.nextDouble(); 
+      if (d <= p)
+      { res = res + 1; }
+    }  
+    return res; 
+  } 
+
+  public double nextNormal(double mu, double vari)
+  { double d = 0.0; 
+    int i = 0; 
+    for ( ; i < 12; i++) 
+    { d = d + this.nextDouble(); }
+    d = d - 6; 
+    return mu + d*Math.sqrt(vari); 
+  }
+
+  public double nextLogNormal(double mu, double vari)
+  { double d = nextNormal(mu,vari); 
+    return Math.exp(d); 
+  }
+
+  public double nextUniform(double lwr,double upr)
+  { double result = 0.0;
+  
+    final double d = this.nextDouble(); 
+    result = lwr + ( upr - lwr ) * d;
+     
+    return result;
+  }
+
+  public double next()
+  { if (distribution.equals("normal"))
+    { return this.nextNormal(normalMean,normalVariance); }
+    if (distribution.equals("lognormal"))
+    { return this.nextLogNormal(normalMean,normalVariance); }
+    if (distribution.equals("bernoulli"))
+    { return this.nextBernoulli(bernoulliP); } 
+    if (distribution.equals("binomial"))
+    { return this.nextBinomial(binomialN, binomialP); } 
+    if (distribution.equals("uniform")) 
+    { return this.nextUniform(
+               this.getuniformLower(),this.getuniformUpper());
+    } 
+    return this.nextDouble();
+  } 
+
+  public double mean()
+  { if (distribution.equals("normal"))
+    { return normalMean; }
+    if (distribution.equals("lognormal"))
+    { return Math.exp(normalMean + normalVariance/2.0); } 
+    if (distribution.equals("bernoulli"))
+    { return bernoulliP; } 
+    if (distribution.equals("binomial"))
+    { return binomialN*binomialP; } 
+    if (distribution.equals("uniform")) 
+    { return (this.getuniformUpper() + this.getuniformLower()) / 2.0; }
+    return 0.5;
+  } 
+   
+  public double variance()
+  { if (distribution.equals("normal"))
+    { return normalVariance; }
+    if (distribution.equals("lognormal"))
+    { return Math.exp(2*normalMean + 2*normalVariance) - Math.exp(2*normalMean + normalVariance); } 
+    if (distribution.equals("bernoulli"))
+    { return bernoulliP*(1 - bernoulliP); } 
+    if (distribution.equals("binomial"))
+    { return binomialN*binomialP*(1 - binomialP); } 
+    if (distribution.equals("uniform")) 
+    { return ( this.getuniformUpper() - this.getuniformLower() ) / 12.0; } 
+    return 1.0/12;
+  } 
 
 
   public static ArrayList randomiseSequence(ArrayList sq)
@@ -206,6 +421,30 @@ class OclRandom { static ArrayList<OclRandom> OclRandom_allInstances = new Array
     int n = col.size(); 
     int ind = (int) Math.floor(Math.random()*n);
     return col.get(ind); 
+  } 
+
+  public static ArrayList randomElements(ArrayList col, int n)
+  { ArrayList res = new ArrayList(); 
+    int sze = col.size();
+    while (res.size() < n) 
+    { int ind = (int) Math.floor(Math.random()*sze);
+      Object x = col.get(ind);
+      res.add(x); 
+    } 
+    return res;  
+  } 
+
+  public static ArrayList randomUniqueElements(ArrayList col, int n)
+  { ArrayList res = new ArrayList(); 
+    int sze = col.size();
+    while (res.size() < n) 
+    { int ind = (int) Math.floor(Math.random()*sze);
+      Object x = col.get(ind);
+      if (res.contains(x)) { } 
+      else 
+      { res.add(x); } 
+    } 
+    return res;  
   } 
    
 }
--- libraries/oclrandom.py
+++ libraries/oclrandom.py
@@ -14,11 +14,21 @@ def free(x):
 class OclRandom : 
   oclrandom_instances = []
   oclrandom_index = dict({})
+  _defaultInstanceOclRandom = None
 
   def __init__(self):
     self.ix = 0
     self.iy = 0
     self.iz = 0
+    self.distribution = "uniform"
+    self.bernoulliP = 0.0
+    self.binomialN = 1
+    self.binomialP = 0.0
+    self.normalMean = 0.0
+    self.normalVariance = 1.0
+    self.uniformLower = 0.0
+    self.uniformUpper = 1.0
+    self.poissonLambda = 1.0
     OclRandom.oclrandom_instances.append(self)
 
   def newOclRandom(n = 0) :
@@ -35,7 +45,79 @@ class OclRandom :
     result = rd
     return result
 
-  def setSeed(self, n) :
+  def newOclRandom_Seed(n = int(time.time())) : 
+    rd = createOclRandom()
+    rd.ix = (n % 30269)
+    rd.iy = (n % 30307)
+    rd.iz = (n % 30323) 
+    return rd
+
+  def newOclRandomBernoulli(p) : 
+    rd = createOclRandom()
+    rd.ix = 1001
+    rd.iy = 781
+    rd.iz = 913 
+    rd.distribution = "bernoulli"
+    rd.bernoulliP = p
+    return rd
+
+  def newOclRandomBinomial(n,p) : 
+    rd = createOclRandom()
+    rd.ix = 1001
+    rd.iy = 781
+    rd.iz = 913 
+    rd.distribution = "binomial"
+    rd.binomialN = n
+    rd.binomialP = p
+    return rd
+
+  def newOclRandomNormal(mu,vari) : 
+    rd = createOclRandom()
+    rd.ix = 1001
+    rd.iy = 781
+    rd.iz = 913 
+    rd.distribution = "normal"
+    rd.bernoulliP = 0.0
+    rd.normalMean = mu
+    rd.normalVariance = vari
+    return rd
+
+  def newOclRandomLogNormal(mu,vari) : 
+    rd = createOclRandom()
+    rd.ix = 1001
+    rd.iy = 781
+    rd.iz = 913 
+    rd.distribution = "lognormal"
+    rd.bernoulliP = 0.0
+    rd.normalMean = mu
+    rd.normalVariance = vari
+    return rd
+
+  def newOclRandomUniform(lwr,upr) :
+    result = None
+    rd = createOclRandom()
+    rd.ix = 1001
+    rd.iy = 781
+    rd.iz = 913
+    rd.distribution = "uniform"
+    rd.uniformLower = lwr
+    rd.uniformUpper = upr
+    result = rd
+    return result
+
+  def newOclRandomPoisson(lm) :
+    result = None
+    rd = createOclRandom()
+    rd.ix = 1001
+    rd.iy = 781
+    rd.iz = 913
+    rd.distribution = "poisson"
+    rd.poissonLambda = lm
+    result = rd
+    return result
+
+
+  def setSeed(self, n = int(time.time())) :
     self.ix = (n % 30269)
     self.iy = (n % 30307)
     self.iz = (n % 30323) 
@@ -67,7 +149,7 @@ class OclRandom :
   def nextGaussian(self) :
     result = 0.0
     d = self.nrandom()
-    result = (d/3.0 - 0.5)
+    result = d*2.0 - 3.0
     return result
 
   def nextInt(self, n = 2147483647) :
@@ -85,6 +167,101 @@ class OclRandom :
     if d > 0.5 :
       result = True
     return result
+
+  def nextBernoulli(self,p) :
+    result = 1
+    d = self.nextDouble()
+    if d > p :
+      result = 0
+    return result
+
+  def nextBinomial(self,n,p) :
+    result = 0
+    for ind in range(0,n) : 
+      d = self.nextDouble()
+      if d <= p :
+        result = result + 1
+    return result
+
+  def nextNormal(self, mu, vari) :
+    d = 0.0 
+    i = 0 
+    while i < 12 : 
+      d = d + self.nextDouble()
+      i = i + 1
+    d = d - 6 
+    return mu + d*math.sqrt(vari)
+
+
+  def nextLogNormal(self, mu, vari) :
+    d = 0.0
+    d = self.nextNormal(mu,vari) 
+    return math.exp(d) 
+
+
+  def nextUniform(self, lwr, upr) :
+    result = 0.0
+    d = 0.0
+    d = self.nextDouble()
+    result = lwr + (upr - lwr) * d
+    return result
+
+  def nextPoisson(self, lam) :
+    result = 0.0
+    x = 0.0
+    p = math.exp((-lam))
+    s = p
+    u = self.nextDouble()
+    while u > s :
+      x = x + 1
+      p = p * lam/x
+      s = s + p
+    return x
+
+  def next(self) : 
+    if self.distribution == "normal" :
+      return self.nextNormal(self.normalMean, self.normalVariance)
+    if self.distribution == "lognormal" :
+      return self.nextLogNormal(self.normalMean, self.normalVariance)
+    if self.distribution == "bernoulli" :
+      return self.nextBernoulli(self.bernoulliP)
+    if self.distribution == "binomial" :
+      return self.nextBinomial(self.binomialN, self.binomialP)
+    if self.distribution == "uniform" :
+      return self.nextUniform(self.uniformLower, self.uniformUpper)
+    if self.distribution == "poisson" :
+      return self.nextPoisson(self.poissonLambda)
+    return self.nextDouble()
+
+  def mean(self) : 
+    if self.distribution == "normal" :
+      return self.normalMean
+    if self.distribution == "lognormal" :
+      return math.exp(self.normalMean + self.normalVariance/2.0)
+    if self.distribution == "bernoulli" :
+      return self.bernoulliP
+    if self.distribution == "binomial" :
+      return self.binomialN*self.binomialP
+    if self.distribution == "uniform" :
+      return (self.uniformUpper + self.uniformLower)/2.0
+    if self.distribution == "poisson" :
+      return self.poissonLambda
+    return 0.5
+
+  def variance(self) : 
+    if self.distribution == "normal" :
+      return self.normalVariance
+    if self.distribution == "lognormal" :
+      return math.exp(2*self.normalMean + 2*self.normalVariance) - math.exp(2*self.normalMean + self.normalVariance)
+    if self.distribution == "bernoulli" :
+      return (self.bernoulliP)*(1 - self.bernoulliP)
+    if self.distribution == "binomial" :
+      return self.binomialN*self.binomialP*(1 - self.binomialP)
+    if self.distribution == "uniform" :
+      return (self.uniformUpper - self.uniformLower)/12.0
+    if self.distribution == "poisson" :
+      return self.poissonLambda
+    return 1.0/12
 
   def randomiseSequence(sq) : 
     ln = len(sq)
@@ -109,17 +286,48 @@ class OclRandom :
       return None
     x = int(random.random()*ln)
     return col[x]
-    
+
+  def randomUniqueElements(col,n) : 
+    return random.sample(col,n)    
+
+  def randomElements(col,n) : 
+    return random.choices(col,k=n)    
+
+  def defaultInstanceOclRandom() : 
+    if OclRandom._defaultInstanceOclRandom == None : 
+      OclRandom._defaultInstanceOclRandom = OclRandom.newOclRandom_Seed()
+    return OclRandom._defaultInstanceOclRandom
 
   def killOclRandom(oclrandom_x) :
     oclrandom_instances = ocl.excludingSet(oclrandom_instances, oclrandom_x)
     free(oclrandom_x)
 
-def createOclRandom():
+def createOclRandom() :
   oclrandom = OclRandom()
   return oclrandom
 
-def allInstances_OclRandom():
+def allInstances_OclRandom() :
   return OclRandom.oclrandom_instances
+
+
+
+# rr = OclRandom.newOclRandomLogNormal(0,1)
+
+# print(rr.next())
+# print(rr.next())
+# print(rr.next())
+# print(rr.next())
+# print(rr.next())
+# print(rr.next())
+
+# rr = OclRandom.newOclRandomBinomial(5,0.3)
+
+# print(rr.nextBinomial(5,0.3))
+# print(rr.nextBinomial(5,0.3))
+# print(rr.nextBinomial(5,0.3))
+# print(rr.nextBinomial(5,0.3))
+# print(rr.nextBinomial(5,0.3))
+
+
 
 
