2c869969ad2e9c79596493c331584140624790ba
Zishan Rahman <zishanfrahman@outlook.com>
2024-12-29 13:50:27 +0000

    Implement subMatrix method in matrixlib.py

--- libraries/matrixlib.py
+++ libraries/matrixlib.py
@@ -25,4 +25,12 @@ class MatrixLib :
         result.append(m[integer])
     return result
 
+  def subMatrix(m: list[list], rows: list[int], cols: list[int]) -> list:
+    result = []
+    for row in rows:
+      if 0 <= row <= len(m) - 1: # OCL starts indexing at 1, Python starts indexing at 0
+        result.append(subRows(m[row], cols))
+    return result
+
+
 print(MatrixLib.matrixMultiplication([[1,2], [3,4]], [[5,6], [7,8]]))
