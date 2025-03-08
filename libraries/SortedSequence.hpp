701740427a0f3e9f9b51190b232b2b08cc399a41
Kevin Lano <kevin.lano@kcl.ac.uk>
2025-03-08 15:12:29 +0000

    SortedSequence for C++19

--- libraries/SortedSequence.hpp
+++ libraries/SortedSequence.hpp
@@ -1,3 +1,5 @@
+// C++19
+
 template<class _T>
 class SortedSequence {
 private: 
@@ -21,6 +23,11 @@ public:
     {
         auto pos = std::lower_bound(elements->begin(), elements->end(), x);
         elements->insert(pos, x); 
+    }
+
+    _T at(int i)
+    {
+        return elements->at(i);
     }
 
     void excluding(_T x)
@@ -114,5 +121,97 @@ public:
       if (sze == 0) { return NULL; } 
       return elements->at(sze-1);
     }
-};
 
+    SortedSequence<_T>* subSequence(int i, int j)
+    { /* OCL indexing i, j: 1..n */
+
+        int n = elements->size();
+        if (n == 0)
+        {
+            return new SortedSequence<_T>();
+        }
+
+        if (i < 1) { i = 1; }
+
+        vector<_T>* elems = new vector<_T>();
+        SortedSequence<_T>* res = new SortedSequence<_T>(); 
+
+        for (int k = i-1; k < j && k < n; k++)
+        {
+            elems->push_back(elements->at(k)); 
+        }
+        res->elements = elems; 
+        return res; 
+    }
+
+    SortedSequence<_T>* front()
+    {
+        int n = elements->size(); 
+        if (n == 0)
+        {
+            return new SortedSequence<_T>();
+        }
+        return subSequence(1, n - 1); 
+    }
+
+    SortedSequence<_T>* tail()
+    {
+        int n = elements->size();
+        if (n == 0)
+        {
+            return new SortedSequence<_T>();
+        }
+        return subSequence(2, n);
+    }
+
+    SortedSequence<_T>* select(std::function<bool(_T)> f)
+    {
+        vector<_T>* elems = new vector<_T>(); 
+
+        for (int i = 0; i < elements->size(); ++i)
+        {
+            _T x = elements->at(i); 
+            if (f(x))
+            {
+                elems->push_back(x); 
+            }
+        }
+
+        SortedSequence<_T>* res = new SortedSequence<_T>();
+        res->elements = elems; 
+        return res;
+    }
+
+    SortedSequence<_T>* reject(std::function<bool(_T)> f)
+    {
+        vector<_T>* elems = new vector<_T>();
+
+        for (int i = 0; i < elements->size(); ++i)
+        {
+            _T x = elements->at(i);
+            if (f(x)) {} 
+            else
+            {
+                elems->push_back(x);
+            }
+        }
+
+        SortedSequence<_T>* res = new SortedSequence<_T>();
+        res->elements = elems;
+        return res;
+    }
+
+    template<class R>
+    vector<R>* collect(std::function<R(_T)> f)
+    {
+        vector<R>* res = new vector<R>();
+        for (int i = 0; i < elements->size(); i++)
+        {
+            _T x = elements->at(i); 
+            res->push_back(f(x));
+        }
+
+        return res;
+    }
+
+};
